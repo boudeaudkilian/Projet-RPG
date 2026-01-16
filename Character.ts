5be6b82154bf6ba6dcfa165236039d281e38ae17
bkilian <kilian.boudeaud@ynov.com>
2026-01-16 09:45:45 +0100

    Class Chara fonctionel

--- Character.ts
+++ Character.ts
@@ -6,52 +6,20 @@ class Character {
     speed : number;
     healthMax : number;
     healthCurrent : number;
-    
-    const warrior = new Character();
-    warrior.name = "Adorn the Brave";
-    warrior.attackPhysic = 20;
-    warrior.attackMagic = 5;
-    warrior.speed = 20;
-    warrior.healthMax = 150;
-    warrior.healthCurrent = 150;
 
-    const paladin = new Character();
-    paladin.name = "Brutus the Just";
-    paladin.attackPhysic = 10;
-    paladin.attackMagic = 10;
-    paladin.speed = 10;
-    paladin.healthMax = 200;
-    paladin.healthCurrent = 200;
-
-    const mage = new Character();
-    mage.name = "Celina the Wise";
-    mage.attackPhysic = 5;
-    mage.attackMagic = 30;
-    mage.speed = 20;
-    mage.healthMax = 120;
-    mage.healthCurrent = 120;
-
-    const barbarian = new Character();
-    barbarian.name = "Drogon the Fierce";
-    barbarian.attackPhysic = 50;
-    barbarian.attackMagic = 5;
-    barbarian.speed = 20;
-    barbarian.healthMax = 120;
-    barbarian.healthCurrent = 120;
-
-    const priest = new Character();
-    priest.name = "Dorina the Kind";
-    priest.attackPhysic = 10;
-    priest.attackMagic = 10;
-    priest.speed = 20;
-    priest.healthMax = 150;
-    priest.healthCurrent = 150;
-
-    const robber = new Character();
-    robber.name = "Calvin the Swift";
-    robber.attackPhysic = 30;
-    robber.attackMagic = 5;
-    robber.speed = 30;
-    robber.healthMax = 120;
-    robber.healthCurrent = 120;
+    constructor(name: string, attackPhysic: number, attackMagic: number, speed: number, healthMax: number, healthCurrent: number) {
+    this.name = name;
+    this.attackPhysic = attackPhysic;
+    this.attackMagic = attackMagic;
+    this.speed = speed;
+    this.healthMax = healthMax;
+    this.healthCurrent = healthMax;
+    }
 }
+
+    const warrior = new Character("Adorn the Brave", 20, 5, 20, 150, 150);
+    const paladin = new Character("Brutus the Just", 10, 10, 10, 200, 200);
+    const mage = new Character("Celina the Wise", 5, 30, 20, 120, 120);
+    const barbarian = new Character("Drogon the Fierce", 50, 5, 20, 120, 120);
+    const priest = new Character("Dorina the Kind", 10, 10, 20, 150, 150);
+    const robber = new Character("Calvin the Swift", 30, 5, 30, 120, 120);
