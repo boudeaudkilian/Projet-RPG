eb41adb883669a8f6e83df0941073be5a642569b
dewen <ewen.denoual@ynov.com>
2026-01-27 11:45:11 +0100

    [fix] menu, add promises system

--- menu.ts
+++ menu.ts
@@ -1,75 +1,78 @@
 import * as readline from 'node:readline'
 
-const menu = (txt : string[], nb_ansers : number) : number => {
+export const menu = (txt : string[], nb_ansers : number) : Promise<number> => {
     let rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-        rl.question( "\n\n\n" + txt[0] + "\n\n\n", (answer) => {
-            switch(answer.toLowerCase()) {
-            case '1': 
-                if (nb_ansers >= 1) {
-                    console.log(txt[1]);
-                    rl.close();
-                    return 1;
-                }/* falls through */
-            case '2':
-                if (nb_ansers >= 2) {
-                    console.log(txt[2]);
-                    rl.close();
-                    return 2;
-                }/* falls through */
-            case '3':
-                if (nb_ansers >= 3) {
-                    console.log(txt[3]);
-                    rl.close();
-                    return 3;
-                }/* falls through */
-            case '4':
-                if (nb_ansers >= 4) {
-                    console.log(txt[4]);
-                    rl.close();
-                    return 4;
-                }/* falls through */
-            case '5':
-                if (nb_ansers >= 5) {
-                    console.log(txt[5]);
-                    rl.close();
-                    return 5;
-                }/* falls through */
-            case '6':
-                if (nb_ansers >= 6) {
-                    console.log(txt[6]);
-                    rl.close();
-                    return 6;
-                }/* falls through */
-            case '7':
-                if (nb_ansers >= 7) {
-                    console.log(txt[7]);
-                    rl.close();
-                    return 7;
-                }/* falls through */
-            case '8':
-                if (nb_ansers >= 8) {
-                    console.log(txt[8]);
-                    rl.close();
-                    return 8;
-                }/* falls through */
-            case '9':
-                if (nb_ansers >= 9) {
-                    console.log(txt[9]);
-                    rl.close();
-                    return 9;
-                }/* falls through */
-            default:
-                console.log('Invalid answer!');
-                rl.close();
-                return -1;
+    let ret_value = 0
+    return new Promise((resolve) => {
+    rl.question( "\n\n\n" + txt[0] + "\n\n\n", (answer) => {
+        switch(answer.toLowerCase()) {
+        case '1': 
+            if (nb_ansers >= 1) {
+                console.log(txt[1]);
+                ret_value = 1;
             }
-        });
-    return 0
+            break
+        case '2':
+            if (nb_ansers >= 2) {
+                console.log(txt[2]);
+                ret_value = 2;
+            }
+            break
+        case '3':
+            if (nb_ansers >= 3) {
+                console.log(txt[3]);
+                ret_value = 3; 
+            }
+            break
+        case '4':
+            if (nb_ansers >= 4) {
+                console.log(txt[4]);
+                ret_value = 4; 
+            }
+            break
+        case '5':
+            if (nb_ansers >= 5) {
+                console.log(txt[5]);
+                ret_value = 5; 
+            }
+            break
+        case '6':
+            if (nb_ansers >= 6) {
+                console.log(txt[6]);
+                ret_value = 6; 
+            }
+            break
+        case '7':
+            if (nb_ansers >= 7) {
+                console.log(txt[7]);
+                ret_value = 7; 
+            }
+            break
+        case '8':
+            if (nb_ansers >= 8) {
+                console.log(txt[8]);
+                ret_value = 8; 
+            }
+            break            
+        case '9':
+            if (nb_ansers >= 9) {
+                console.log(txt[9]);
+                ret_value = 9; 
+            }
+            break
+        default:
+            console.log('Invalid answer!');
+            ret_value = -1;
+        }
+        rl.close();
+        resolve(ret_value)
+    });
+    });
 }
 
-menu(["Bienvenue dans le Jeu, pas grand chose a voir pour le moment, | 1 : Restez | 2 : Partir |  ", 
-    'Bah ça change pas que y a rien, Au revoir', 
-    'Au revoir'], 3)
+// menu(["Bienvenue dans le Jeu, pas grand chose a voir pour le moment, | 1 : Restez | 2 : Partir |  ", 
+//     'Bah ça change pas que y a rien, Au revoir', 
+//     'Au revoir'], 3)
