7b4ad1ae14efc2a44df8bcc648bc809602730f3e
dewen <ewen.denoual@ynov.com>
2026-02-10 08:59:44 +0100

    [maj] link acceuil menu to battle systme, need to add attaque

--- fight.ts
+++ fight.ts
@@ -1,5 +1,4 @@
 import { Character } from "./Character.ts"
-import { charalist } from "./Character.ts"
 
 const Attack = (chara : Character, _chara_list : Character[]) : void => {
     console.log(chara.name)
@@ -21,16 +20,14 @@ const Speed_def = (chara_list : Character[], indice_spe : number) : Character[] 
     }
 }
 
-const Fight = (chara_list : Character[]) : void => {
+export async function Fight(chara_list : Character[]) : Promise<void> {
     let indice_spe = 100
     for (indice_spe = 100; indice_spe > 0; indice_spe--) {
         const buf = Speed_def(chara_list, indice_spe)
         if (buf) {
             for (const element of buf) {
-                Attack(element, chara_list)
+                await Attack(element, chara_list)
             }
         }
     }
 }
-
-Fight(charalist)
