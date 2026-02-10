a55888630cd15476837f9525e8f9e0d58f93483d
dewen <ewen.denoual@ynov.com>
2026-02-10 10:00:21 +0100

    [add] stats fct

--- accueil.ts
+++ accueil.ts
@@ -2,6 +2,9 @@ import { Fight } from "./fight.ts";
 import { menu } from "./menu.ts"
 import { charalist } from './Character.ts';
 
+async function stats() {
+    const val = await menu(["De qui veut tu voir les stats"] 0)    
+}
 async function Accueil() {
     while (true) {
         const val = await menu(["Vous arrivez à la taverne et, après une petite chope, vous décidez ce que vous allez faire ensuite : \n|1| : Faire le point sur votre équipement\n|2| : Se reposer (15 po)\n|3] : Partir à l'avanture !\n|4| : Rentrer au bercail (Fermer le jeu)",
